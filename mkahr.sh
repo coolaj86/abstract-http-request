3c9fb9ce0f4f68417352814be241ac5c14dc07a9
AJ ONeal <coolaj86@gmail.com>
2010-10-25 01:03:18 -0600

    better abstraction, works in node, but still depends on jQuery in browser

--- mkahr.sh
+++ mkahr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat vendor/global-es5.js vendor/futures.promise.js lib/ahr.js > lib/ahr.all.js
+cat vendor/global-es5.js vendor/url.js vendor/futures.promise.js lib/ahr.js > lib/ahr.all.js
 #cat vendor/require-kiss.js vendor/global-es5.js vendor/futures.promise.js lib/ahr.js > lib/ahr.all.js
