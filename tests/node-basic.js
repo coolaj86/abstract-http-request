966bd7b24c51dfee6cb59a048c62acb3f9af61dd
AJ ONeal <coolaj86@gmail.com>
2010-10-30 18:54:44 -0600

    now passes node-request tests

--- tests/node-basic.js
+++ tests/node-basic.js
@@ -1,26 +1,46 @@
 (function () {
-  var ahr = require('./ahr'),
+  var ahr = require('../lib/ahr'),
     tests;
 
   tests = [
     {
       key: "simple200",
-      url: "http://www.google.com",
+      uri: "http://www.google.com",
       regex: /Google Search/
     },
     {
+      key: "port80",
+      uri: "http://www.google.com:80",
+      regex: /Google Search/
+    },
+    {
+      key: "port5984",
+      uri: "http://mikeal.couchone.com:5984",
+      regex: /{"couchdb":"Welcome","version":"1/
+    },
+    {
+      key: "POST json",
+      uri: "http://mikeal.couchone.com:5984/testjs",
+      options: {
+        method:'POST',
+        headers: { 'content-type': 'application/json', 'accept': 'application/json'},
+        body: { _id: Math.floor(Math.random()*100000000).toString() }
+      },
+      regex: /"ok":true/
+    },
+    {
       key: "redirect301",
-      url: "http://coolaj86.github.com",
+      uri: "http://coolaj86.github.com",
       regex: /CoolAJ86/
     },
     {
       key: "encodedUrl",
-      url: "http://www.google.com/search?hl=en&client=firefox-a&rls=org.mozilla%3Aen-US%3Aofficial&q=%22json+to+x-www-form-urlencoded%22&aq=f&aqi=&aql=&oq=&gs_rfai=",
+      uri: "http://www.google.com/search?hl=en&client=firefox-a&rls=org.mozilla%3Aen-US%3Aofficial&q=%22json+to+x-www-form-urlencoded%22&aq=f&aqi=&aql=&oq=&gs_rfai=",
       regex: /x-www-form-urlencoded/
     },
     {
       key: "encodedParams",
-      url: "http://www.google.com/search",
+      uri: "http://www.google.com/search",
       params: { 
         hl: "en",
         client: "firefox-a",
@@ -36,7 +56,7 @@
     },
     {
       key: "jsonp",
-      url: "http://api.flickr.com/services/feeds/photos_public.gne?format=json",
+      uri: "http://api.flickr.com/services/feeds/photos_public.gne?format=json",
       params: { tags: "cat", tagmode: "any", "jsoncallback": "jsonp_" + (new Date()).valueOf() },
       options: { jsonp: "jsoncallback" },
       regex: /jsonp_\d+\(/
@@ -46,7 +66,7 @@
   tests.forEach(function (test) {
     // As simple as it gets
     test.options = test.options || {};
-    test.options.url = test.url;
+    test.options.uri = test.uri;
     test.options.params = test.params;
     ahr.http(test.options).when(function (err, data, response) {
       if (err || !data || !data.match(test.regex)) {
