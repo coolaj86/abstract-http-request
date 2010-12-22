e73141346a9c28e360f4f763d409b0f92a594b87
AJ ONeal <coolaj86@gmail.com>
2010-12-21 19:30:31 -0700

    added futures as submodule

--- mkahr.sh
+++ mkahr.sh
@@ -3,8 +3,8 @@
 cat vendor/require-kiss.js\
   vendor/global-es5.js \
   vendor/url.js \
-  vendor/futures.private.js \
-  vendor/futures.promise.js \
+  vendor/futures/lib/private.js \
+  vendor/futures/lib/promise.js \
   lib/ahr.js \
     > lib/ahr.all.js
 #cat vendor/require-kiss.js vendor/global-es5.js vendor/futures.promise.js lib/ahr.js > lib/ahr.all.js
