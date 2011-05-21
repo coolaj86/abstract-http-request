5256985298e2fb38aef693f166bc01b9cccd54c1
AJ ONeal <coolaj86@gmail.com>
2011-05-21 14:00:00 -0600

    generate test dat files

--- tests/generate-test-upload-files.js
+++ tests/generate-test-upload-files.js
@@ -2,20 +2,25 @@
   "use strict";
 
   var fs = require('fs')
-    , i
-    , j
-    , num = 16
-    , data = ''
-    , c
     , bank = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-  for (i = 0; i < num; i += 1) {
-    c = bank[i];
-    for (j = 0; j < 1023; j += 1) {
-      data += c;
+  function writeFile(name, num) {
+    var i, j, c, bindex, data = '';
+
+    for (i = 0; i < num; i += 1) {
+      bindex = i % bank.length;
+      c = bank[bindex];
+      for (j = 0; j < 1023; j += 1) {
+        data += c;
+      }
+      data += '\n';
     }
-    data += '\n';
+
+    fs.writeFileSync(name, data);
   }
 
-  fs.writeFileSync(num + 'kb.dat', data);
+  writeFile('256kb.dat', 256);
+  writeFile('16kb.dat', 16);
+  writeFile('2k_b.dat', 2);
+  writeFile('1k_a.dat', 1);
 }());
