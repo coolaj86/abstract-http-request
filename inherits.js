be9b4974d5895fa5e7313044ecca799e4c570f0e
AJ ONeal <coolaj86@gmail.com>
2013-01-09 11:33:59 -0700

    added inherits

---
+++ inherits.js
@@ -0,0 +1,18 @@
+/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true unused:true undef:true*/
+(function () {
+  "use strict";
+
+  function inherits(ctor, superCtor) {
+    ctor.super_ = superCtor;
+    ctor.prototype = Object.create(superCtor.prototype, {
+      constructor: {
+        value: ctor,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+  }
+
+  module.exports = inherits;
+}());
